4901e6e4a465c8dabc237104f288f1f3c02cfd09
LeVietHa05 <ha.leviet.0504@gmail.com>
2026-03-17 12:10:00 +0700

    fix wrong padding

--- app/baccaohoc/page.tsx
+++ app/baccaohoc/page.tsx
@@ -78,7 +78,7 @@ export default function Baccaohoc() {
 
             <Thanhtich data={thanhtichData} initItemperpage={10} />
 
-            <div className="-mt-32 text-[#ECFEB0] relative z-1 bg-[#004DEE] py-12 px-4 lg:px-32 pt-44 rounded-[32px] lg:rounded-[60px]">
+            <div className="-mt-32 text-[#ECFEB0] relative z-1 bg-[#004DEE] py-12 px-4 lg:px-32 pt-44 rounded-[32px] lg:rounded-[60px] text-justify">
                 <div className={`${SVNHelveticaNeueHeavy.className} font-black text-base lg:text-2xl `}>
                     Inception mang giải pháp học thuật toàn diện cho hành trình <br className="hidden lg:block"/> chinh phục học bổng cao học tại các trường đại học <br className="hidden lg:block"/> hàng đầu thế giới.
                 </div>
@@ -115,7 +115,7 @@ export default function Baccaohoc() {
                         </div>
                     </div>
 
-                    <div className={`bg-[#FDFFD6] lg:bg-[#ECFEB0] font-bold flex items-center gap-4 lg:gap-12 rounded-[30px] p-4 pb-20 lg:pb-4 -mt-20 lg:mt-0`}>
+                    <div className={`bg-[#FDFFD6] lg:bg-[#ECFEB0] font-bold flex items-center gap-4 lg:gap-12 rounded-[30px] p-2 lg:p-4 pb-20 lg:pb-4 -mt-20 lg:mt-0`}>
                         <div className={`${SVNHelveticaNeueHeavy.className} size-12 lg:size-15 pb-2  aspect-1/1 rounded-full border text-center flex items-center justify-center text-2xl`}>
                             2
                         </div>
@@ -128,7 +128,7 @@ export default function Baccaohoc() {
                             </div>
                         </div>
                     </div>
-                    <div className={`bg-[#ECFEB0] lg:bg-[#FDFFD6] font-bold flex items-center gap-4 lg:gap-12 rounded-[30px] p-4 -mt-20 lg:mt-0 pb-20 lg:pb-4`}>
+                    <div className={`bg-[#ECFEB0] lg:bg-[#FDFFD6] font-bold flex items-center gap-4 lg:gap-12 rounded-[30px] p-2 lg:p-4 -mt-20 lg:mt-0 pb-20 lg:pb-4`}>
                         <div className={`${SVNHelveticaNeueHeavy.className} size-12 lg:size-15 pb-2  aspect-1/1 rounded-full border text-center flex items-center justify-center text-2xl`}>
                             3
                         </div>
@@ -141,7 +141,7 @@ export default function Baccaohoc() {
                             </div>
                         </div>
                     </div>
-                    <div className={`bg-[#FDFFD6] font-bold flex items-center gap-4 lg:gap-12 rounded-[30px] p-4 -mt-20 lg:mt-0`}>
+                    <div className={`bg-[#FDFFD6] font-bold flex items-center gap-4 lg:gap-12 rounded-[30px] p-2 lg:p-4 -mt-20 lg:mt-0`}>
                         <div className={`${SVNHelveticaNeueHeavy.className} size-12 lg:size-15 pb-2  aspect-1/1 rounded-full border text-center flex items-center justify-center text-2xl`}>
                             4
                         </div>
